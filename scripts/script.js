90566cc7e851410e7e4f5088c6d4e18f664e61d3
Kaique Fernando Monteoliva Ananias <fkaique678@gmail.com>
2023-06-02 19:54:50 -0300

    Primeiro Commit

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+let menu = document.getElementById("menu_links")
+let menu_barras = document.getElementById("menu_barras")
+
+function mostrarMenu() {
+    if (window.getComputedStyle(menu).display == 'none'){
+        //Mostre o menu
+        menu.style.display = "flex";
+        menu_barras.setAttribute("aria-expanded", "true")
+        menu_barras.setAttribute("aria-label", "fechar o menu")
+    }else{
+        //Esconda o menu
+        menu.style.display = "none";
+
+        menu_barras.setAttribute("aria-expanded", "false")
+        menu_barras.setAttribute("aria-label", "abrir o menu")
+    }
+}
